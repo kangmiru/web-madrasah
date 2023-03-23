c7074685683846d0e2b8e44e34a7274f3e90a80f
kangmiru <mirufadz25@gmail.com>
2023-03-23 08:37:56 +0700

    menyelesaikan bagian sekolah dan meresponsivekan halaman

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -56,122 +56,206 @@
     </div>
 
     <!-- profil singkat -->
-    <div class="profil" style="background-color: #fff;">
-        <div class="row justify-content-center me-0">
-            <div class="col-md-5 my-5">
-                <h3>Lorem, ipsum dolor.</h3>
-                <p>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis consequatur accusamus, maiores aspernatur quia sapiente ex cumque officia! Obcaecati, quo alias blanditiis eligendi quae maiores unde asperiores incidunt voluptatem, nemo praesentium fugiat eos ab facere repellat corporis quaerat. Mollitia amet sed illo autem nam voluptatem similique, inventore facilis repellendus quaerat! Tempora, eos. Quidem totam modi dicta earum sint, velit repudiandae ducimus quasi facere beatae ea molestiae, soluta minima quisquam magni deleniti, vitae quas assumenda. Ratione, ipsum aperiam dolore voluptate, iste quos officia temporibus, autem rem tenetur nam deserunt est vitae molestias repellat laborum ex pariatur reprehenderit unde enim explicabo modi.
-                </p>
-                <button class="selengkapnya btn btn-primary text-center"><a href="" style="text-decoration:none; color:white;">
-                <span>selengkapnya </span> 
-                </a></button>
-            </div>
-            <div class="col-md-5 my-5 ms-5 g-6">
-                <img class="center mx-auto d-block" src="/img/mad1.jpg" style="height:250px;">
-                <p class="text-center">Lorem, ipsum dolor.</p>
+    <section>
+        <div class="profil" style="background-color: #fff;">
+            <div class="row justify-content-center me-0">
+                <div class="col-md-5 my-5">
+                    <h3>Lorem, ipsum dolor.</h3>
+                    <p>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis consequatur accusamus, maiores aspernatur quia sapiente ex cumque officia! Obcaecati, quo alias blanditiis eligendi quae maiores unde asperiores incidunt voluptatem, nemo praesentium fugiat eos ab facere repellat corporis quaerat. Mollitia amet sed illo autem nam voluptatem similique, inventore facilis repellendus quaerat! Tempora, eos. Quidem totam modi dicta earum sint, velit repudiandae ducimus quasi facere beatae ea molestiae, soluta minima quisquam magni deleniti, vitae quas assumenda. Ratione, ipsum aperiam dolore voluptate, iste quos officia temporibus, autem rem tenetur nam deserunt est vitae molestias repellat laborum ex pariatur reprehenderit unde enim explicabo modi.
+                    </p>
+                    <button class="selengkapnya btn btn-primary text-center">
+                        <a href="" style="text-decoration:none; color:white;">
+                            <span>selengkapnya </span> 
+                        </a>
+                    </button>
+                </div>
+                <div class="col-md-5 my-5 g-6">
+                    <img class="center mx-auto d-block" src="/img/mad1.jpg">
+                    <p class="text-center">Lorem, ipsum dolor.</p>
+                </div>
             </div>
         </div>
-    </div>
+    </section>
 
     <!-- bagian sekolah -->
-    <div class="bagian">
-        <div class="row text-center my-5 me-0">
-            <div class="col-md-3" style="border-right: 2px solid black;">
-                <h5 style="margin-left:9px">Menjadi Bagian Alif Al-ittifaq Boarding School</h5>
-            </div>
-            
-            <div class="col-md-3" style="border-right: 2px solid black;">
-                <img src="" alt="">
-                <p>10</p>
-            </div>
-            
-            <div class="col-md-3" style="border-right: 2px solid black;">
-                <img src="" alt="">
-                <p>10</p>
-            </div>
-            
-            <div class="col-md-3">
-                <img src="" alt="">
-                <p>10</p>
+    <section>
+        <div class="bagian">
+            <div class="row text-center my-5 me-0">
+                <div class="col-md-3" style="border-right: 2px solid black;">
+                    <h5 style="margin-left:9px">Menjadi Bagian Alif Al-ittifaq Boarding School</h5>
+                </div>
+                
+                <div class="col-md-3" style="border-right: 2px solid black;">
+                    <img src="" alt="">
+                    <p>10</p>
+                </div>
+                
+                <div class="col-md-3" style="border-right: 2px solid black;">
+                    <img src="" alt="">
+                    <p>10</p>
+                </div>
+                
+                <div class="col-md-3">
+                    <img src="" alt="">
+                    <p>10</p>
+                </div>
             </div>
         </div>
-    </div>
+    </section>
 
     <!-- alasan -->
-    <div class="alasan" style="background-color:#fff;">
-    <div class="container">
-        <div class="row text-center mb-3 me-0">
-            <div class="col">
-                <h2 class="mt-4">Kenapa Madrasah Alif?</h2>
+    <section>
+        <div class="alasan" style="background-color:#fff;">
+            <div class="container">
+            <div class="row text-center mb-3 me-0">
+                <div class="col">
+                    <h2 class="mt-4">Kenapa Madrasah Alif?</h2>
+                </div>
+            </div>
+            <div class="row pb-5 me-0">
+                <div class="col-md-4 mb-3">
+                    <div class="card alasan" >
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-3">
+                    <div class="card alasan" >
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-3">
+                    <div class="card alasan" >
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
             </div>
         </div>
-        <div class="row pb-5 me-0">
-            <div class="col-md-4 mb-3">
-                <div class="card alasan" >
-                    <div class="card-body">
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4 mb-3">
-                <div class="card alasan" >
-                    <div class="card-body">
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4 mb-3">
-                <div class="card alasan" >
-                    <div class="card-body">
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    </div>
+    </section>
 
     <!-- kata alumni -->
-    <div class="alumni pb-5">
-        <h2 class="text-center py-5">Kata Alumni</h2>
-        <div class="container review">
-            <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-                <div class="carousel-indicators" style="">
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <section>
+        <div class="alumni pb-5">
+            <h2 class="text-center py-5">Kata Alumni</h2>
+            <div class="container review">
+                <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-indicators" style="">
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    </div>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <div class=" text-center pt-3 mb-3">
+                                <h4>Nama</h4>
+                                <p>pekerjaan/jenjang lanjutan</p>
+                                <img src="/img/oke1.png" class="foto-alumni">
+                                <p>lulusan</p>
+                                <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo, vel eius doloribus ea ex consequuntur veniam dignissimos! Aspernatur, numquam perferendis, accusamus magni ratione provident rerum vero ducimus dolorum tempora assumenda, dolor dicta. Distinctio, inventore! Fugit impedit obcaecati eaque, pariatur aspernatur id. Commodi maiores esse aut perspiciatis hic dicta, sed aliquam.</p>
+                             </div>
+                        </div>
+                        <div class="carousel-item">
+                            <div class=" text-center pt-3 mb-3">
+                                <h4>Nama</h4>
+                                <p>pekerjaan/jenjang lanjutan</p>
+                                <img src="/img/oke1.png" class="foto-alumni">
+                                <p>lulusan</p>
+                                <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo, vel eius doloribus ea ex consequuntur veniam dignissimos! Aspernatur, numquam perferendis, accusamus magni ratione provident rerum vero ducimus dolorum tempora assumenda, dolor dicta. Distinctio, inventore! Fugit impedit obcaecati eaque, pariatur aspernatur id. Commodi maiores esse aut perspiciatis hic dicta, sed aliquam.</p>
+                             </div>
+                        </div>
                 </div>
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <div class=" text-center pt-3 mb-3">
-                            <h4>Nama</h4>
-                            <p>pekerjaan/jenjang lanjutan</p>
-                            <img src="/img/oke1.png" class="foto-alumni">
-                            <p>lulusan</p>
-                            <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo, vel eius doloribus ea ex consequuntur veniam dignissimos! Aspernatur, numquam perferendis, accusamus magni ratione provident rerum vero ducimus dolorum tempora assumenda, dolor dicta. Distinctio, inventore! Fugit impedit obcaecati eaque, pariatur aspernatur id. Commodi maiores esse aut perspiciatis hic dicta, sed aliquam.</p>
-                         </div>
-                    </div>
-                    <div class="carousel-item">
-                        <div class=" text-center pt-3 mb-3">
-                            <h4>Nama</h4>
-                            <p>pekerjaan/jenjang lanjutan</p>
-                            <img src="/img/oke1.png" class="foto-alumni">
-                            <p>lulusan</p>
-                            <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo, vel eius doloribus ea ex consequuntur veniam dignissimos! Aspernatur, numquam perferendis, accusamus magni ratione provident rerum vero ducimus dolorum tempora assumenda, dolor dicta. Distinctio, inventore! Fugit impedit obcaecati eaque, pariatur aspernatur id. Commodi maiores esse aut perspiciatis hic dicta, sed aliquam.</p>
-                         </div>
-                    </div>
             </div>
         </div>
-    </div>
+    </section>
 
     <!-- sekolah kami -->
-    <div class="sekolah my-5 pt-2">
-        <h2 class="text-center mt-5">Sekolah Kami</h2>
-        <div class="row">
-            
+    <section>
+        <div class="sekolah py-5 pt-2">
+            <h2 class="text-center mt-5 mb-5">Sekolah Kami</h2>
+            <div class="row me-0 ms-3">
+                <div class="owl-carousel carousel-theme slider">
+                    <div class="col-md-3 mb-3 item ">
+                        <div class="card-sekolah" >
+                            <div class="card sekolah-card"  style="width: 400px;">
+                                <img src="/img/mad1.jpg" class="card-img-top">
+                                <div class="card-body">
+                                    <h5 class="card-title">Madrasah Aliyah</h5>
+                                    <button class="selengkapnya btn btn-primary text-center">
+                                        <a href="" style="text-decoration:none; color:white;">
+                                            <span>selengkapnya </span> 
+                                        </a>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 mb-3 item ">
+                        <div class="card-sekolah">
+                            <div class="card sekolah-card" style="width: 400px;">
+                                <img src="/img/mad1.jpg" class="card-img-top">
+                                <div class="card-body">
+                                    <h5 class="card-title">Madrasah Aliyah</h5>
+                                    <button class="selengkapnya btn btn-primary text-center">
+                                        <a href="" style="text-decoration:none; color:white;">
+                                            <span>selengkapnya </span> 
+                                        </a>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 mb-3 item ">
+                        <div class="card-sekolah">
+                            <div class="card sekolah-card" style="width: 400px;">
+                                <img src="/img/mad1.jpg" class="card-img-top">
+                                <div class="card-body">
+                                    <h5 class="card-title">Madrasah Aliyah</h5>
+                                    <button class="selengkapnya btn btn-primary text-center">
+                                        <a href="" style="text-decoration:none; color:white;">
+                                            <span>selengkapnya </span> 
+                                        </a>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 mb-3 item ">
+                        <div class="card-sekolah">
+                            <div class="card sekolah-card" style="width: 400px;">
+                                <img src="/img/mad1.jpg" class="card-img-top">
+                                <div class="card-body">
+                                    <h5 class="card-title">Madrasah Aliyah</h5>
+                                    <button class="selengkapnya btn btn-primary text-center">
+                                        <a href="" style="text-decoration:none; color:white;">
+                                            <span>selengkapnya </span> 
+                                        </a>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
+    </section>
+
+    <!-- berita madrasah -->
+    <section class="berita py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h2 class="text-center">Berita Terkini</h2>
+                </div>
+            </div>
+        </div>
+    </section>
 
 @endsection
     
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,11 +8,19 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
+    {{-- bootstrap icons --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+
+    {{-- owl carausel --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" integrity="sha512-tS3S5qG0BlhnQROyJXvNjeEM4UpMXHrQfTGmbQ1gKmelCxlSEBUaxhRBj/EFTzpbP4RVSrpEikbmdJobCvhE3g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" integrity="sha512-sMXtMNL1zRzolHYKEujM2AqCLUR9F2C4/05cdbxjjLSRvMQIciEPCQZo++nk7go3BtSuK9kfa/s+a4f4i5pLkw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
     {{-- My CSS --}}
     <link rel="stylesheet" href="/css/background.css">
     <link rel="stylesheet" href="/css/cover.css">
     <link rel="stylesheet" href="/css/tombol.css">
     <link rel="stylesheet" href="/css/foto.css">
+    <link rel="stylesheet" href="/css/responsive.css">
 
     <title>Madrasah Alif | </title>
   </head>
@@ -46,16 +54,25 @@
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
+    {{-- Jquery --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js" integrity="sha512-pumBsjNRGGqkPzKHndZMaAG+bir374sORyzM3uulLV14lN5LyykqNk8eEeUlUkB3U0M4FApyaHraT65ihJhDpQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+
+    {{-- icons --}}
+    <script src="https://unpkg.com/feather-icons"></script>
+
+    {{-- CDN js --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
     {{-- My Script --}}
     <script src="/js/navbar.js"></script>
-
-    {{-- Jquery --}}
-    <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+    <script src="/js/owl-carousel.js"></script>
 
     <!-- Option 2: Separate Popper and Bootstrap JS -->
     <!--
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
     -->
+    
   </body>
 </html>
