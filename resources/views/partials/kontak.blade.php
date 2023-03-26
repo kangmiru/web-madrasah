893b1e0531d908f8a1a07ca10bdebbc731e1fb5e
kangmiru <mirufadz25@gmail.com>
2023-03-26 13:04:16 +0700

    pengerjaan awal partial kontak

--- resources/views/partials/kontak.blade.php
+++ resources/views/partials/kontak.blade.php
@@ -1,7 +1,10 @@
 <section class="kontak">
     <div class="row me-0 alamat bg-light">
-        <div class="col-6 ms-0 text-center " style="font-size : 20px">
-            <p class="lokasi bg-secondary text-white"><i class="bi bi-geo-alt-fill"></i> Ciwidey, Jawa Barat</p>
+        <div class="col-6 ms-0 lokasi text-center bg-secondary" style="font-size : 20px;">
+            <p class="text-white my-auto"><i class="bi bi-geo-alt-fill"></i> Ciwidey, Jawa Barat</p>
         </div>
+        <!-- <div class="col-6 ms-0 email text-center bg-dark" style="font-size : 20px;">
+            <p class="text-white my-auto"><i class="bi bi-geo-alt-fill"></i>madrasahalif@gmail.com</p>
+        </div> -->
     </div>
 </section>
