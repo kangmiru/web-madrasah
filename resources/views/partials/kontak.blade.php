4818f34c16f4a7a3fa3f20897fddbaef492d4e84
kangmiru <mirufadz25@gmail.com>
2023-03-27 09:58:41 +0700

    menyelesaikan partials kontak

--- resources/views/partials/kontak.blade.php
+++ resources/views/partials/kontak.blade.php
@@ -1,10 +1,51 @@
 <section class="kontak">
-    <div class="row me-0">
-        <div class="col-md-4 d-flex justify-content-center">
+    <div class="row me-0 my-5">
+        <div class="col-md-5 d-flex justify-content-center">
             <img src="/../img/logo1.png" alt="">
         </div>
-        <div class="col-md-3 d-flex justify-content-center">
-            
+        <div class="col-md-3 jsutify-content-center">
+            <h4>Media Sosial</h4>
+            <a href="" class="media">
+                <p>
+                    <span class="medsos bi bi-instagram bi-lg" style="width: 500px;"></span>
+                    madrasahalifofc
+                </p>
+            </a>
+            <a href="" class="media">
+                <p>
+                    <span class="medsos bi bi-facebook"></span>
+                    madrasahalifofc
+                </p>
+            </a>
+            <a href="" class="media">
+                <p>
+                    <span class="medsos bi bi-youtube"></span>
+                    madrasahalifofc
+                </p>
+            </a>
+        </div>
+        <div class="col-md-4 justify-content-center">
+            <h4>Kontak</h4>
+            <p>
+                <span class="bi bi-geo-alt-fill"></span>
+                 Alamat
+            </p>
+            <p class="ms-5">Kp.Ciburial, Desa Alamendah, Kec.Rancabali, Kab.Bandung, Jawa Barat</p>
+            <p>
+                <span class="bi bi-telephone-fill"></span>
+                 Telepon
+            </p>
+            <p class="ms-5">(022)78798798</p>
+            <p>
+                <span class="bi bi-envelope-at-fill"></span>
+                Email
+            </p>
+            <p class="ms-5">madrasah@gmail.com</p>
+        </div>
+    </div>
+    <div class="row me-0 bg-dark text-light">
+        <div class="col text-center mt-1">
+            <p>&copy; Madrasah ABS</p>
         </div>
     </div>
 </section>
